9a3b062f50f3da7cea617b2ccf2c6be9a2dd8c12
Jorge-Blanco-42 <66803425+Jorge-Blanco-42@users.noreply.github.com>
2020-08-10 15:05:49 -0600

    upload image de producto

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,12 +4,14 @@ var express  = require('express');
 var Product = require('../controllers/product');
 
 var router = express.Router();
-
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart({uploadDir: './uploads'});
 router.get('/home', Product.home);
 router.post('/test', Product.test);
 router.post('/save-product', Product.addProduct);
 router.get('/get-product/:id?', Product.getProduct);
 router.get('/get-products', Product.getProducts);
+router.post("/upload-image-product/:id", multipartMiddleware, Product.uploadImage);
 router.put('/update-product/:id', Product.updateProduct);
 router.delete('/delete-product/:id', Product.deleteProduct);
 
